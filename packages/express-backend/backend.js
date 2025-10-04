43225a420a86011f820ed34f1c7bc1ff1f09d2b4
Kyle Fan <43425389+kylemfan@users.noreply.github.com>
2025-10-03 17:59:33 -0700

    Added search-by-ID endpoint

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -44,21 +44,31 @@ const users = {
   ],
 };
 
-
 const findUserByName = (name) => {
-    return users["users_list"].filter(
-        (user) => user["name"] === name
-    );
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
+const findUserById = (id) => {
+  return users["users_list"].find((user) => user["id"] === id);
 };
 
 app.get("/users", (req, res) => {
-    const name = req.query.name;
-    if (name != undefined) {
-        let result = findUserByName(name);
-        result = { users_list: result };
-        res.send(result);
-    } else {
-        res.send(users);
-    }
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
 });
 
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found");
+  } else {
+    res.send(result);
+  }
+});
